c6b5d5617367266d999fe9f61d0a5429f87e1b06
Sarah-Marion <devsarahmarion@gmail.com>
2018-03-26 22:27:16 +0300

    overlay screen set to active

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,3 @@ $(document).ready(function() {
     $(".overlay-screen").toggleClass("active")
   })
 })
-$(document).ready(function(){
-  $('.fa-bars').click(function() {
-     $('.overlay-screen').toggleClass('active')
-   })
- })
